1c590f134b9c689ce662251e7deb05d523671650
Smileyman5 <namespaceuser395@gmail.com>
2016-04-14 12:21:36 -0400

    Initial commit for the speech mode again. It has some issues, but at least it's back up.

--- Lemon_Notes/src/SpeechMode.java
+++ Lemon_Notes/src/SpeechMode.java
@@ -1,13 +1,18 @@
+import com.sun.speech.freetts.Voice;
+import com.sun.speech.freetts.VoiceManager;
 import javafx.scene.text.Text;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by alex on 4/7/2016.
  */
 public class SpeechMode extends Mode
 {
+    private ExecutorService executor = Executors.newSingleThreadExecutor();
     public SpeechMode()
     {
         super("Text To Speech", "<speak>", "Takes in words",
@@ -29,7 +34,42 @@ public class SpeechMode extends Mode
                 textArray.add(text);
             }
         }
+        executor.execute(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                // listAllVoices();
+                String voiceName = "kevin16";
+                VoiceManager voiceManager = VoiceManager.getInstance();
+                Voice helloVoice = voiceManager.getVoice(voiceName);
+                if(helloVoice == null) {
+                    System.err.println("Cannot find a voice named " + voiceName + ".  Please specify a different voice.");
+                    System.exit(1);
+                }
+
+                helloVoice.allocate();
+                for(String text: textArray)
+                {
+                    if(!text.equals(""))
+                        helloVoice.speak(text);
+                }
+                helloVoice.deallocate();
+            }
+        });
 
         return textAreaBoxData;
+    }
+
+    public static void listAllVoices() {
+        System.out.println();
+        System.out.println("All voices available:");
+        VoiceManager voiceManager = VoiceManager.getInstance();
+        Voice[] voices = voiceManager.getVoices();
+
+        for(int i = 0; i < voices.length; ++i) {
+            System.out.println("    " + voices[i].getName() + " (" + voices[i].getDomain() + " domain)");
+        }
+
     }
 }
